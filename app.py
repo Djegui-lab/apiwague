9f068ec711b4d2c3fb5beae2d3b9acf723420428
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2024-02-01 20:31:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -379,7 +379,7 @@ def reset_form_state():
             "CB2": "", "Primme_mensuelle": "", "TotalFrais": "", "ID": "", "Email": ""}
 
 # Sidebar content
-st.sidebar.title("Ajouter une nouvelle donnée!")
+st.sidebar.title("Ajouter une nouvelle donnée🚗")
 
 # Initialiser le formulaire dans la session si ce n'est pas déjà fait
 if "form_state" not in st.session_state:
