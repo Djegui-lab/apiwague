57db5989dd72117840811e694c5b862c4fef88d6
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2024-03-03 16:11:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ with st.sidebar.form(key="idea_form"):
     st.session_state.form_state["Primme_mensuelle"] = st.text_input("Montant Prime_mensuelle (optional)", value=st.session_state.form_state["Primme_mensuelle"], placeholder="Prime_mensuelle")
     st.session_state.form_state["TotalFrais"] = st.text_input("total frais (optional)", value=st.session_state.form_state["TotalFrais"], placeholder="Montant Frais")
     st.session_state.form_state["ID"] = st.text_input("ID (optional)", value=st.session_state.form_state["ID"], placeholder="Code_courtier")
-    st.session_state.form_state["Email"] =  st.multiselect("Statut-Contrat",["validé", "retracté"], placeholder="Statut-Contrat")
+    st.session_state.form_state["Statut-Contrat"] =  st.multiselect("Statut-Contrat",["validé", "retracté"], placeholder="Statut-Contrat")
 
     submit_button = st.form_submit_button(label="Envoyé 🚀")
 
